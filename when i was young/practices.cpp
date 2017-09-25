779a3ebc96cdd3851aa8878cda42c48c47e45a75
luengzeegin <493406032@qq.com>
2017-09-25 23:23:27 +0800

    general update

--- when i was young/practices.cpp
+++ when i was young/practices.cpp
@@ -1,14 +1,23 @@
-// new 带参创造类的多个对象
-// length 数组长度(对象个数)
-Foo *foo = reinterpret_cast<Foo*>(new char[length * sizeof(Foo)]);
-for (size_t idx = 0; idx < length; ++idx)
 {
-	new(foo + idx)Foo(ival);
+	// new 带参创造类的多个对象
+	// length 数组长度(对象个数)
+	Foo *foo = reinterpret_cast<Foo*>(new char[length * sizeof(Foo)]);
+	for (size_t idx = 0; idx < length; ++idx)
+	{
+		new(foo + idx)Foo(ival);
+	}
+	//delete
+	for (size_t idx = 0; idx < length; ++idx)
+	{
+		foo[idx]->~Foo();
+	}
+		delete foo;
 }
 
-//delete
-for (size_t idx = 0; idx < length; ++idx)
 {
-	foo[idx]->~Foo();
+	// 根据值 / 子串构造类（可以是派生关系）
+	// map / factory 模式，其实两种是一个东西，只是IFactory对应class Factory : public IFactory
+	map<string, function<shared_ptr<T>()>>;
+	注册function -> [=/&]() {return make_shared<T>()};
 }
-delete foo;
+
